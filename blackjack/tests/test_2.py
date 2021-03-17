9a0fffce3d1479a9f3d9aa3711e4f692b2101695
kaseymallette <kcmmallette@gmail.com>
2021-03-16 20:04:24 -0400

    Commit

--- blackjack/tests/test_2.py
+++ blackjack/tests/test_2.py
@@ -1,15 +1,18 @@
-# Import necessary libraries
-from run_test import Test
-
 # Set up directory
+from run_test import Test
 Test().find_dir()
 
+# Import Path
+from file_path import Path
+
 # Set variables
-test_shuffle = [1, 3, 5, 9, 15, 'part_1', 'part_2', 'casino']
+test_shuffle = [1, 4, 7, 11, 'part_1', 'part_2', 'casino']
 test_shoes = 96
-run_time = 3
+run_time = 7
+shoe_fh = Path('shoe_test_2.csv').path
+hand_fh = Path('hand_test_2.csv').path
 
 # Run test
 for shuffle in test_shuffle:
     for i in range(run_time):
-        Test().run_test(shuffle, test_shoes)
+        Test().run_test(shuffle, test_shoes, shoe_fh, hand_fh)
