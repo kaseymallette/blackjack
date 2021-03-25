be664777483396b379681f38f3580a68018f9bf8
KaseyMallette <kcmmallette@gmail.com>
2021-03-25 18:44:10 -0400

    Commit

--- blackjack/analysis/eda_shoe_data.py
+++ blackjack/analysis/eda_shoe_data.py
@@ -46,10 +46,22 @@ scaler = preprocessing.RobustScaler().fit(X)
 scaled_data = scaler.transform(X)
 
 # Create dataframes of 10% and 90% quartiles 
-win_quant_03 = shoe_df['win_pct'].quantile(0.03)
+win_quant_05 = shoe_df['win_pct'].quantile(0.05)
+win_quant_95 = shoe_df['win_pct'].quantile(0.95)
 win_quant_97 = shoe_df['win_pct'].quantile(0.97)
-quant_03_df = shoe_df[shoe_df['win_pct'] < win_quant_03]
+quant_05_df = shoe_df[shoe_df['win_pct'] < win_quant_05]
+quant_95_df = shoe_df[shoe_df['win_pct'] > win_quant_95]
 quant_97_df = shoe_df[shoe_df['win_pct'] > win_quant_97]
+
+# Create dataframes of 10% and 90% quartiles 
+win_quant_10 = shoe_df['win_pct'].quantile(0.1)
+win_quant_90 = shoe_df['win_pct'].quantile(0.9)
+win_quant_25 = shoe_df['win_pct'].quantile(0.25)
+win_quant_75 = shoe_df['win_pct'].quantile(0.75)
+quant_10_df = shoe_df[shoe_df['win_pct'] < win_quant_10]
+quant_90_df = shoe_df[shoe_df['win_pct'] > win_quant_90]
+boxplot_df = shoe_df[(shoe_df['win_pct'] > win_quant_25) & (shoe_df['win_pct'] < win_quant_75)]
+
 
 # Create dataframes of outliers
 std_3 = win_pct_std*3
@@ -122,11 +134,6 @@ for var in check_var:
 
 #%%
 # Use seaborn to create a histogram and kernel density estimate of win_pct
-sns.displot(data=quant_90_df, x='win_pct', hue='shuffle_method', kde=True,
-            multiple='stack')
-
-sns.displot(data=quant_10_df, x='win_pct', hue='shuffle_method', kde=True,
-            multiple='stack')
 
 sns.displot(data=shoe_df, x='win_pct', hue='shuffle_method', kde=True,
             multiple='stack')
@@ -136,21 +143,89 @@ sns.displot(data=shoe_df, x='win_pct', hue='shuffle_method', kind='kde',
 
 #%%
 
+describe = shoe_df.describe()
+describe_clumpy = riffle_clumpy.describe()
+describe_python = python_shuffle.describe()
 
-outlier_high_clumpy = outliers_high_df[outliers_high_df['shuffle_method'] =='riffle_clumpy']
-outlier_high_comp = outliers_high_df[outliers_high_df['shuffle_method'] == 'python']
-t1 = outlier_high_clumpy['win_pct']
-t2 = outlier_high_comp['win_pct']
-ttest = stats.ttest_ind(t1,t2)
-print("T-Test of outliers with two different shuffles")
-print("Python vs riffle clumpy")
-print(ttest)
+quant_10_clump = quant_10_df[quant_10_df['shuffle_method'] =='riffle_clumpy']
+quant_10_py = quant_10_df[quant_10_df['shuffle_method'] == 'riffle_perfect']
+t1 = quant_10_clump['dealer_bust']
+t2 = quant_10_py['dealer_bust']
+ttest_1 = stats.ttest_ind(t1,t2)
+print("T-Test of dealer_bust in 10th quantile of clumpy vs perfect")
+print(ttest_1)
+print("Significant!\n")
+
+quant_10_clump = quant_10_df[quant_10_df['shuffle_method'] =='riffle_clumpy']
+quant_10_py = quant_10_df[quant_10_df['shuffle_method'] == 'riffle_perfect']
+t1 = quant_10_clump['win_push_pct']
+t2 = quant_10_py['win_push_pct']
+ttest_2 = stats.ttest_ind(t1,t2)
+print("T-Test of win_push_pct in 10th quantile of clumpy vs perfect")
+print(ttest_2)
+print("Significant!\n")
+
+
+
+outlier_high_clump = outliers_high_df[outliers_high_df['shuffle_method'] =='riffle_clumpy']
+outlier_high_per = outliers_high_df[outliers_high_df['shuffle_method'] == 'python']
+t1 = outlier_high_clump['dealer_low_card']
+t2 = outlier_high_per['dealer_low_card']
+ttest_3 = stats.ttest_ind(t1,t2)
+print("T-Test of dealer low card in high outliers of clumpy vs python")
+print(ttest_3)
+print("Significant!\n")
+
 
 quant_97_clump = quant_97_df[quant_97_df['shuffle_method'] =='riffle_clumpy']
 quant_97_py = quant_97_df[quant_97_df['shuffle_method'] == 'python']
-t1 = quant_97_clump['win_pct']
-t2 = quant_97_py['win_pct']
-ttest = stats.ttest_ind(t1,t2)
-print("T-Test of outliers with two different shuffles")
-print("Quant 95")
-print(ttest)
+t1 = quant_97_clump['win_push_pct']
+t2 = quant_97_py['win_push_pct']
+ttest_4 = stats.ttest_ind(t1,t2)
+print("\nT-Test of outliers with two different shuffles")
+print(ttest_4)
+
+
+quant_90_clump = quant_90_df[quant_90_df['shuffle_method'] =='riffle_clumpy']
+quant_90_py = quant_90_df[quant_90_df['shuffle_method'] == 'python']
+t1 = quant_90_clump['dealer_low_card']
+t2 = quant_90_py['dealer_low_card']
+ttest_5 = stats.ttest_ind(t1,t2)
+print("\nT-Test of outliers with two different shuffles")
+print(ttest_5)
+
+quant_90_clump = quant_90_df[quant_90_df['shuffle_method'] =='riffle_clumpy']
+quant_90_py = quant_90_df[quant_90_df['shuffle_method'] == 'python']
+t1 = quant_90_clump['dealer_bust']
+t2 = quant_90_py['dealer_bust']
+ttest_6 = stats.ttest_ind(t1,t2)
+print("\nT-Test of outliers with two different shuffles")
+print(ttest_6)
+
+
+t1 = riffle_clumpy['push']
+t2 = python_shuffle['push']
+ttest_7 = stats.ttest_ind(t1,t2)
+print("\nT-Test of two different shuffles and the number of hands pushed")
+print(ttest_7)
+
+
+boxplot_clump = boxplot_df[boxplot_df['shuffle_method'] == 'riffle_clumpy']
+boxplot_py = boxplot_df[boxplot_df['shuffle_method'] == 'python']
+t1 = boxplot_clump['player_win']
+t2 = boxplot_py['player_win']
+ttest_8 = stats.ttest_ind(t1,t2)
+print("\nT-Test of two different shuffles and the number of hands pushed")
+print(ttest_8)
+
+boxplot_clump = boxplot_df[boxplot_df['shuffle_method'] == 'riffle_clumpy']
+boxplot_py = boxplot_df[boxplot_df['shuffle_method'] == 'riffle_perfect']
+t1 = boxplot_clump['push']
+t2 = boxplot_py['push']
+ttest_9 = stats.ttest_ind(t1,t2)
+print("\nT-Test of two different shuffles and the number of hands pushed")
+print(ttest_9)
+
+
+describe_b_clumpy = boxplot_clump.describe()
+describe_b_py = boxplot_py.describe()
